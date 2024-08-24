e7bf69f75d9678b907799a79a18ae5283b0a7a7d
James Curtis <james.curtis@kcl.ac.uk>
2024-08-24 01:07:15 +0100

    Define Navbar and overarching page structure
    Create 3 pages, home, about and blog.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+
+<Navbar rounded color="form">
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-3xl font-bold dark:text-white">JC</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl>
+		<NavLi href="/" active={true}>Home</NavLi>
+		<NavLi href="/about">About</NavLi>
+		<NavLi href="/blog">Blog</NavLi>
+	</NavUl>
+</Navbar>
+<slot></slot>
